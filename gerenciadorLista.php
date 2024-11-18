e630a7dce5bc2b81d5cd3b5f5a309df048e4cdaa
Hariston Nunes Macedo <85318716+HaristonNuMacedo@users.noreply.github.com>
2024-11-18 08:33:49 -0300

    Update Lista Design

--- gerenciadorLista.php
+++ gerenciadorLista.php
@@ -64,9 +64,17 @@
                 <div class="card-item id<?php echo $task['task_id'] ?>">
                     <h2 style="margin-bottom: 5px"><?php echo $task['task_name'] ?></h2>
                         <p><?php echo $task['task_description'] ?></p>
-                    <div style="display:inline-flex; margin-top: 5px;">
-                        <span2 style="font-size: 14px; font-weight: 700; top: 3px;">Prioridade:</span2>
-                        <p2 style="font-size: 13px; margin-left: 8px; position:relative; top: -4px"><?php echo $task['task_prioridade'] ?></p2>
+                    <div class="prioridadeList" style="display:inline-flex;">
+                        <span2 style="font-size: 14px; font-weight: 700;"><i class="fas fa-flag" 
+                        <?php $task['task_prioridade'] ;
+                        if ($task['task_prioridade'] == 'Urgente') {
+                            $colorFlag = '#ff0000';
+                        } else if ($task['task_prioridade'] == 'Alta') { $colorFlag = '#c0c000';}
+                        else if ($task['task_prioridade'] == 'Normal') { $colorFlag = '#0000ff';}
+                        else if ($task['task_prioridade'] == 'Baixa') { $colorFlag = '#008000';}
+                        else { $colorFlag = '#000';}
+                        ?> style="color: <?php echo $colorFlag; ?>; "></i></span2>
+                        <p2 style="font-size: 13px; margin-left: 8px; position:relative;"><?php echo $task['task_prioridade'] ?></p2>
                     </div>
                 </div>
             <?php } } ?>

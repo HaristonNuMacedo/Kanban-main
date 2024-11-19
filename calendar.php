95fb61188daf4a32e278319722f2a0a9084f04bd
Hariston Nunes Macedo <85318716+HaristonNuMacedo@users.noreply.github.com>
2024-11-19 14:13:19 -0300

    Calendar Sprint

---
+++ calendar.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="/assets/css/custom.css" rel="stylesheet">
+    <title>FullCalendar</title>
+</head>
+
+<body>
+
+    <div id='calendar'></div>
+
+    <script src='./assets/js/index.global.min.js'></script>
+    <script src='./assets/js/locales-all.global.min.js'></script>
+    <script src='./assets/js/custom.js'></script>
+
+</body>
+
+</html>
